70831b3bcfa84f7801233a6303a8356abdfe1816
marlon nino <mvnino.halcyondigital@gmail.com>
2024-03-18 22:08:27 +0800

    feat: update todolist component

--- react-todo/src/components/Navbar.jsx
+++ react-todo/src/components/Navbar.jsx
@@ -1,25 +1,14 @@
 const Navbar = () => {
   return (
-    <nav className="bg-slate-900">
-      <div className="sm: container mx-auto flex items-center justify-around space-x-10 py-4 text-white lg:justify-start">
-        <a href="#">
-          <img
-            className="h-8"
-            src="https://via.placeholder.com/150"
-            alt="Sample logo"
-          />
-        </a>
-        <ul className="flex space-x-3 text-gray-300">
-          <li>
-            <a className="text-white" href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#">Tasks</a>
-          </li>
-        </ul>
-      </div>
+    <nav className="navbar">
+      <a href="#" className="navbar-brand">LOGO</a>
+      <ul className="navbar-menu">
+        <li>
+          <a href="#" className="navbar-menu-item">Home</a>
+          <a href="#" className="navbar-menu-item">Tasks</a>
+          <a href="#" className="navbar-menu-item">About</a>
+        </li>
+      </ul>
     </nav>
   );
 };
